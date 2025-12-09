9b35789ec5d42584718f529f7774db445bde4bec
n_n <nursultanzhantuar@gmail.com>
2025-12-09 14:47:45 +0500

    redesign front page

--- frontend/housing-app/src/app/app.routes.ts
+++ frontend/housing-app/src/app/app.routes.ts
@@ -14,7 +14,6 @@ export const routes: Routes = [
             {
                 path: '',
                 component: HomePage,
-                canActivate: [canActivateAuth]
             },
             {
                 path: 'login',
--- frontend/housing-app/src/app/pages/home-page/home-page.ts
+++ frontend/housing-app/src/app/pages/home-page/home-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Button } from '../../common-ui/button/button';
+import { Router } from '@angular/router';
+import { HeaderComponent } from '../../common-ui/header/header.component';
+import { FooterComponent } from '../../common-ui/footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Button, HeaderComponent, FooterComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
 export class HomePage {
+  router: Router = inject(Router);
 
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  redirectToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
